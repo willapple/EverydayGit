6730989a75b418172f22ed7748c29e6ea2abeee9
willapple <willapple@aliyun.com>
2016-03-04 14:41:19 +0800

    modify file 2016-3-4.java

--- 2016-3-4.java
+++ 2016-3-4.java
@@ -1,1 +1,16 @@
-//测试远程代码编写下载到本地
+﻿package com;
+
+/**
+ * java split 函数的用法
+ * @author William
+ *
+ */
+public class Test {
+	public static void main(String[] args) {
+		String[] s = ("one two three four five six seven eight nine ten").split(" ");
+		for (int i = 0; i < s.length; i++){
+			System.out.println(s[i]);
+		}
+	}
+	
+}
